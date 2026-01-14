ce6a023f60e5d43aa058910567de50e7d69c1daa
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-01-14 18:43:34 +0800

    feat: add motion dependency and update header component to use HyperText for improved styling

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -1,12 +1,15 @@
 import Link from "next/link"
 import { ModeToggle } from "../mode-toggle"
+import { HyperText } from "../ui/hyper-text"
 
 function Header() {
     return (
-        <header className='wrapper font-bold -tracking-widest sm:text-5xl text-2xl
+        <header className='wrapper font-bold -tracking-wide sm:text-5xl text-2xl
     backdrop-blur shadow-accent'>
             <div className="flex gap-2 items-center justify-between">
-                <Link className="" href='/'>HDA</Link>
+                <Link className="" href='/'>
+                    <HyperText>HDA</HyperText>
+                </Link>
                 <div className="text-lg">
                     <ModeToggle />
                 </div>
