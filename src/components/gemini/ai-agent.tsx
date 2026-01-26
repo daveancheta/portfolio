bfdbd5775c73407d5d009e15cd7915da0c07673b
Heaven Dave Ancheta <daveancheta8@gmail.com>
2026-01-26 14:50:08 +0800

    feat: implement animation effects for AIAgent visibility toggle

--- src/components/gemini/ai-agent.tsx
+++ src/components/gemini/ai-agent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Button } from '../ui/button'
 import { Send } from 'lucide-react'
 import ChatContainer from './chat-container'
@@ -6,12 +6,32 @@ import { cn } from '@/lib/utils'
 
 function AIAgent() {
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const containerRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (isOpen) {
+            containerRef?.current?.classList.remove("hidden")
+
+            setTimeout(() => {
+                containerRef?.current?.classList.remove("opacity-0", "scale-0")
+                containerRef?.current?.classList.add("opacity-100", "scale-100")
+            }, 100)
+        } else {
+            containerRef?.current?.classList.remove("opacity-100", "scale-100")
+            containerRef?.current?.classList.add("opacity-0", "scale-0")
+
+            setTimeout(() => {
+                containerRef?.current?.classList.add("hidden")
+            }, 300)
+        }
+    })
 
     return (
         <div className='fixed sm:bottom-10 bottom-5 flex sm:justify-self-end justify-self-center sm:mr-10 cursor-none z-20'>
             <div className='flex flex-col gap-2 sm:items-end items-center'>
-                <div className={cn("transition-all duration-300 sm:origin-bottom-right origin-bottom ease-in-out", isOpen ? "opacity-100 scale-100" : "opacity-0 scale-0 hidden sm:flex")}>
-                    <ChatContainer isOpen={isOpen} setIsOpen={setIsOpen}/>
+                <div className="transition-all duration-300 sm:origin-bottom-right origin-bottom ease-in-out opacity-0 scale-0 hidden"
+                    ref={containerRef} >
+                    <ChatContainer isOpen={isOpen} setIsOpen={setIsOpen} />
                 </div>
                 <Button variant="default"
                     className='rounded-full p-6 cursor-none w-fit'
